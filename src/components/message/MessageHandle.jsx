74aebe8b2a4ff0055c24c463c6c993c3057fcec6
Brian Henry <brianhenrytech@gmail.com>
2025-01-31 11:37:17 -0600

    updated MessageHandle Component

--- src/components/message/MessageHandle.jsx
+++ src/components/message/MessageHandle.jsx
@@ -3,24 +3,24 @@ import BotMessage from "./BotMessage"
 
 const MessageHandle = ({ message, currentUser, getAndSetChatMessages }) => {
   return (
-    {
-      message.userId === 0 ?
-        <BotMessage
-          key={message.id}
-          message={message}
-          currentUser={currentUser}
-          translate={false}
-        />
-        :
-        <Message
-          key={message.id}
-          message={message}
-          currentUser={currentUser}
-          translate={false}
-          getAndSetChatMessages={getAndSetChatMessages}
-        />
-    }
+    message.userId === 0 ? (
+      <BotMessage
+        key={message.id}
+        message={message}
+        currentUser={currentUser}
+        translate={false}
+      />
+    ) : (
+      <Message
+        key={message.id}
+        message={message}
+        currentUser={currentUser}
+        translate={false}
+        getAndSetChatMessages={getAndSetChatMessages}
+      />
+    )
   )
+
 }
 
 export default MessageHandle
